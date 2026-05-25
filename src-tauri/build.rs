ea05c6c490c4f25b4b801dffd0552ca7b7a2d157
liangyue <1115060927@qq.com>
2026-05-25 11:07:44 +0800

    fix：修复打包问题，转为GUI

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_OS");
+    println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_ENV");
+    println!("cargo:rerun-if-env-changed=PROFILE");
+
     tauri_build::build();
+
+    let is_windows_msvc = env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows")
+        && env::var("CARGO_CFG_TARGET_ENV").as_deref() == Ok("msvc");
+    let is_release = env::var("PROFILE").as_deref() == Ok("release");
+
+    if is_windows_msvc && is_release {
+        println!("cargo:rustc-link-arg-bin=flowtype=/SUBSYSTEM:WINDOWS");
+        println!("cargo:rustc-link-arg-bin=flowtype=/ENTRY:mainCRTStartup");
+    }
 }
